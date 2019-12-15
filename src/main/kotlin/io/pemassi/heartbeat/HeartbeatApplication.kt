513a5cfd23a7302bcc473fa56b84e2a348730067
Kyungyoon Kim <ruddbs5302@gmail.com>
2019-12-14 23:12:25 -0700

    Initial Commit

---
+++ src/main/kotlin/io/pemassi/heartbeat/HeartbeatApplication.kt
@@ -0,0 +1,11 @@
+package io.pemassi.heartbeat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeartbeatApplication
+
+fun main(args: Array<String>) {
+	runApplication<HeartbeatApplication>(*args)
+}
---
+++ src/test/kotlin/io/pemassi/heartbeat/HeartbeatApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.pemassi.heartbeat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeartbeatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
